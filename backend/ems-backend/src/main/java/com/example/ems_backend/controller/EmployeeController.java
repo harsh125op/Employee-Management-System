f6cc0e3633bb9a79af4ae696516a18a7eae458e3
Harsh Mahanti <72938822+harsh125op@users.noreply.github.com>
2025-06-17 00:55:53 +0530

    Update EmployeeController.java

--- backend/ems-backend/src/main/java/com/example/ems_backend/controller/EmployeeController.java
+++ backend/ems-backend/src/main/java/com/example/ems_backend/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "http://***.0.0.*:****")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
